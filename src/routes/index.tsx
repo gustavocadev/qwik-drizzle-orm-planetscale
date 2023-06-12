959a0bb45aa06d90217e26507b60ab5ea9af1f95
gustavocadev <gustavocadev@gmail.com>
2023-06-11 21:33:25 -0500

    feat: add logout button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,31 @@
 import { component$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { Form, routeAction$, routeLoader$ } from '@builder.io/qwik-city';
 import { auth } from '~/lib/lucia';
 
 export const useUserLoader = routeLoader$(async (event) => {
   const authRequest = auth.handleRequest(event);
-  const { user } = await authRequest.validateUser();
-  if (!user) {
+  const validate = await authRequest.validateUser();
+  if (!validate.user) {
     throw event.redirect(303, '/login');
   }
 
-  console.log('user', user);
+  return validate.user;
+});
 
-  return user;
+export const useLogoutUserAction = routeAction$(async (values, event) => {
+  const authRequest = auth.handleRequest(event);
+  const { session } = await authRequest.validateUser();
+
+  if (!session) throw event.redirect(302, '/login');
+
+  await auth.invalidateSession(session.sessionId);
+  authRequest.setSession(null);
+  throw event.redirect(302, '/login');
 });
 
 export default component$(() => {
   const userLoader = useUserLoader();
+  const logoutUserAction = useLogoutUserAction();
   return (
     <div class="m-8">
       <pre>
@@ -25,6 +35,12 @@ export default component$(() => {
       <h1 class="text-3xl">
         if you see this, it means that you have successfully authenticated
       </h1>
+
+      <Form action={logoutUserAction}>
+        <button class="btn btn-error" type="submit">
+          Logout
+        </button>
+      </Form>
     </div>
   );
 });
