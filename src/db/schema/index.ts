4ff55852d45151f668ad82ec3722c58dad3967ab
gustavocadev <gustavocadev@gmail.com>
2023-08-13 11:53:49 -0500

    chore: update schemas

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,6 +1,6 @@
 import { mysqlTable, bigint, varchar } from 'drizzle-orm/mysql-core';
 
-export const user = mysqlTable('auth_user', {
+export const users = mysqlTable('auth_user', {
   id: varchar('id', {
     length: 15, // change this when using custom user ids
   }).primaryKey(),
@@ -16,7 +16,7 @@ export const user = mysqlTable('auth_user', {
   }),
 });
 
-export const session = mysqlTable('user_session', {
+export const sessions = mysqlTable('user_session', {
   id: varchar('id', {
     length: 128,
   }).primaryKey(),
@@ -32,7 +32,7 @@ export const session = mysqlTable('user_session', {
   }).notNull(),
 });
 
-export const key = mysqlTable('auth_key', {
+export const keys = mysqlTable('auth_key', {
   id: varchar('id', {
     length: 255,
   }).primaryKey(),
